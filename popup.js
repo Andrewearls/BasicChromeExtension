ade3e36795f79242fa9acb095a5db04f58546b8a
Andrew Earls <andrewearls@rocketmail.com>
2019-07-16 13:53:18 -0500

    tab update event listener

--- popup.js
+++ popup.js
@@ -3,8 +3,6 @@ let get_url = document.getElementById('get_url');
 
 
 get_url.onclick = function(element) {
-	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-	  	var url = tabs[0].url;	  	
-	});
-    console.log(url);
+	
+	
 };
